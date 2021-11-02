849bb2a1f4b8b198bee9ad4ae6e6841467a20b1b
Stasiu <wolanski58@gmail.com>
2021-11-02 21:05:17 +0000

    Hehe

--- app/static/canvas.js
+++ app/static/canvas.js
@@ -11,4 +11,15 @@ function animate__c() {
     ctx.moveTo(100, 50); // Move the pen to x=100, y=50.
     ctx.lineTo(300, 150); // Draw a line to x=300, y=150.
     ctx.stroke(); // Render the path
+    draw_axes();
+}
+
+function draw_axes(){
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.moveTo(0,0.8*height);
+    ctx.lineTo(width,0.8*height);
+    ctx.moveTo(0.5*width,0);
+    ctx.lineTo(0.5*width,height);
+    ctx.stroke();
 }
