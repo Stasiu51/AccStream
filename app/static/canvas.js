87583c9154bdd32c0410f01317f7f2842a304b9c
Stasiu <wolanski58@gmail.com>
2021-11-02 12:37:16 +0000

    Hehe

--- app/static/canvas.js
+++ app/static/canvas.js
@@ -1,3 +1,4 @@
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 function animate__c() {
@@ -8,4 +9,4 @@ function animate__c() {
     ctx.moveTo(100, 50); // Move the pen to x=100, y=50.
     ctx.lineTo(300, 150); // Draw a line to x=300, y=150.
     ctx.stroke(); // Render the path
-}
+}-
