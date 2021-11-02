76db793a874d7cf657665a522033e11aa2563f0b
Stasiu <wolanski58@gmail.com>
2021-11-02 12:21:30 +0000

    Hehe

--- app/static/canvas.js
+++ app/static/canvas.js
@@ -1,9 +1,11 @@
 const ctx = canvas.getContext('2d');
 
-ctx.strokeStyle = 'black';
-ctx.lineWidth = 5;
+function animate() {
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = 5;
 
-ctx.beginPath(); // Start a new path
-ctx.moveTo(100, 50); // Move the pen to x=100, y=50.
-ctx.lineTo(300, 150); // Draw a line to x=300, y=150.
-ctx.stroke(); // Render the path
+    ctx.beginPath(); // Start a new path
+    ctx.moveTo(100, 50); // Move the pen to x=100, y=50.
+    ctx.lineTo(300, 150); // Draw a line to x=300, y=150.
+    ctx.stroke(); // Render the path
+}
